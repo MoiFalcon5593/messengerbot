00ccc0905ba57e704869fd37f75ed54fd0056dfe
MoiFalcon5593 <53017102+MoiFalcon5593@users.noreply.github.com>
2019-09-10 18:06:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+# encoding: utf-8
 import webapp2
+import json
 import logging
+from google.appengine.api import urlfetch
 
 VERIFY_TOKEN = "facebook_verification_token"
+ACCESS_TOKEN = "EAAKASDPk4TgBAIYZAZCcEUczNUHq7laEQaMK6gp05ZAlHtupRjVTjue9Qyn2V2YZBJ6bUpnAZA7szNGQy72NeTkqJ5flkzdOXJjfCuwTSIB2y8zY9QgjWnswXzH3vmP8VFC9B3fPlalZBOZA0SQZBr9wj9q7uO1OZAnhKSvZAVWBagMzN3ndS96IMi"
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -23,11 +13,71 @@ class MainPage(webapp2.RequestHandler):
         mode = self.request.get("hub.mode")
         if mode == "subscribe":
             challenge = self.request.get("hub.challenge")
-            verify_token = self.request.get("hub.verify.token")
+            verify_token = self.request.get("hub.verify_token")
             if verify_token == VERIFY_TOKEN:
                 self.response.write(challenge)
         else:
-            self.response.write("OKeyes")       
+            self.response.write("Ok")
+
+    def post(self):
+        data = json.loads(self.request.body)
+        logging.info("Data obtenida desde Messenger: %s", data)
+
+        if data ["object"] == "page":
+
+            for entry in data ["entry"]:
+                for messaging_event in entry["messaging"]:
+                    sender_id = messaging_event["sender"]["id"]
+
+                    if messaging_event.get("message"):
+                        message = messaging_event['message']
+                        message_text = message.get('text','')
+                        logging.info("Mensaje obtenido desde msgr: %s", message_text)
+                        send_message(sender_id, "hola, soy un bot") 
+
+                    if messaging_event.get("postback"):
+                        logging.info("Post-back")
+
+def send_message(recipient_id, message_text):
+    logging.info("Enviando mensaje a %r: %s", recipient_id, message_text)
+    headers = {
+        "Content-Type": "application/json"
+    }
+    #message = {"text": message_text}
+    possible_answers = ["Opcion A", "Opcion B", "Opcion C"]
+    message = get_postback_buttons_message(message_text, possible_answers)
+
+    raw_data = {
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": message
+    }
+    data = json.dumps(raw_data)
+    r = urlfetch.fetch("https://graph.facebook.com/v2.6/me/messages?access_token=%s" % ACCESS_TOKEN,
+                       method=urlfetch.POST, headers=headers, payload=data)
+    if r.status_code != 200:
+        logging.error("Error %r enviando mensaje: %s", r.status_code)
+
+def get_postback_buttons_message(message_text, possible_answers):
+    buttons = []
+    for answer in possible_answers:
+        buttons.append({
+            "type": "postback,",
+            "title": answer,
+            "payload": answer
+        })
+
+    return {
+            "attachment":{
+                "type":"template",
+                "payload": {
+                    "template_type": "button",
+                    "text": message_text,
+                    "buttons": buttons
+                }
+            }
+        }
 
 
 app = webapp2.WSGIApplication([
